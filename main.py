e02b262a71da25e2108d06768fc89a415d601cfe
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-18 17:10:44 -0700

    works, now adding the finishing touches

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-from machine import Pin,SPI,PWM
-import utime, requests, network
+from machine import Pin,SPI,PWM,reset
+import utime, requests, network, _thread
 
-ssid = input("ssid> ")
-password = input("password> ")
-
-def connect():
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(ssid)
-    while wlan.isconnected() == False:
-        print('Waiting for connection..')
-        utime.sleep(1)
-    # print(wlan.ifconfig())
-    ip = wlan.ifconfig()[0]
-    print(f'Connected on {ip}')
-    return ip
-
-connect()
-
+print("starting!")
 MOSI = 11
 SCK = 10    
 RCLK = 9
+ssid = ""
+password = ""
+with open("wifisettings.txt", "r") as f:
+    ssid, password = f.read().split("\n")
 
-SEG8Code = [0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71]
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        utime.sleep(1)
+    print(wlan.ifconfig())
+
 class LED_8SEG():
     def __init__(self):
         self.rclk = Pin(RCLK,Pin.OUT)
@@ -30,7 +27,10 @@ class LED_8SEG():
         self.spi = SPI(1)
         self.spi = SPI(1,1000_000)
         self.spi = SPI(1,10000_000,polarity=0, phase=0,sck=Pin(SCK),mosi=Pin(MOSI),miso=None)
-        self.SEG8=SEG8Code
+        self.SEG8 = [0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71]
+        self.to_write = [self.SEG8[0], self.SEG8[0], self.SEG8[0], self.SEG8[0]]
+        self.loading = True
+        _thread.start_new_thread(self.write, ())
     def write_cmd(self, Reg, Seg):    
         self.rclk(1)
         self.spi.write(bytearray([Reg]))
@@ -38,13 +38,27 @@ class LED_8SEG():
         self.rclk(0)
         utime.sleep(0.002)
         self.rclk(1)
+    def write(self):
+        while (1):
+            self.write_cmd(0XFE,self.to_write[0])
+            self.write_cmd(0XFD,self.to_write[1])
+            self.write_cmd(0XFB,self.to_write[2])
+            self.write_cmd(0XF7,self.to_write[3])
 
-if __name__=='__main__':
-    LED = LED_8SEG()
-    req = requests.get("https://hackhour.hackclub.com/api/clock/U06MGGA9XM1")
-    print(req)
-    while(1):
-        LED.write_cmd(0XFE,LED.SEG8[4]|0X80)
-        LED.write_cmd(0XFD,LED.SEG8[3]|0X80)
-        LED.write_cmd(0XFB,LED.SEG8[2]|0X80)
-        LED.write_cmd(0XF7,LED.SEG8[1]|0X80)
+
+
+LED = LED_8SEG()
+
+connect()
+req = requests.get("https://hackhour.hackclub.com/api/clock/U06MGGA9XM1")
+time = int(req.text)
+
+lasttime = utime.time()*1000
+while(1):
+    time -= utime.time()*1000 - lasttime
+    lasttime = utime.time()*1000
+    min = time//1000//60
+    sec = (time//1000)%60
+    LED.to_write = [LED.SEG8[min//10], LED.SEG8[min%10]|0x80, LED.SEG8[sec//10], LED.SEG8[sec%10]]
+    # LED.to_write = [0b10000000, 0b01000000, 0b00100000, 0b00010000]
+    utime.sleep(0.1)
